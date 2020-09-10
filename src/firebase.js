439bd50873b51da6f9013784377ae371f924af34
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-10 10:09:30 +0530

    made it ready for deployment, and added keys to gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,4 @@
-import firebase from "firebase";
-
-const firebaseapp = firebase.initializeApp({
-  apiKey: "AIzaSyBqK1blsGT_onwWDX__Eyvykhr4OZ_PBC4",
-  authDomain: "todo-23851.firebaseapp.com",
-  databaseURL: "https://todo-23851.firebaseio.com",
-  projectId: "todo-23851",
-  storageBucket: "todo-23851.appspot.com",
-  messagingSenderId: "559857881379",
-  appId: "1:559857881379:web:f4aea43a68dae630a8bc96",
-  measurementId: "G-W6L0CYXZV3",
-});
+import firebaseapp from "./keys";
 
 const db = firebaseapp.firestore();
 export default db;
